e606ca2c7fdcf754ea47c9e164cf2170e9b06973
Shadow Wolfrain <allenverdot1224356@gmail.com>
2021-09-28 15:53:16 -0500

    New commit

--- FurryGayDattingApp.Client/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ FurryGayDattingApp.Client/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace FurryGayDattingApp.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 1 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 2 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 3 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 4 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 5 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 6 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 7 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 8 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 9 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using FurryGayDattingApp.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Allen\RiderProjects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
+#line 10 "F:\CyberDragon Studio\C# Programs\Projects\FurryGayDattingApp\FurryGayDattingApp.Client\_Imports.razor"
 using FurryGayDattingApp.Client.Shared;
 
 #line default
---
+++ FurryGayDattingApp.Common/Domains/ProfileModal.cs
@@ -0,0 +1,10 @@
+﻿namespace FurryGayDattingApp.Common.Domains
+{
+    public class ProfileModal
+    {
+        public int Id { get; set; }
+        public string AccountName { get; set; }
+        public string BasicDescription { get; set; }
+        
+    }
+}
